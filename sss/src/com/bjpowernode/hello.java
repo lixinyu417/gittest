157dcb3a4d2f647aa6423e845948468a37fabb09
lixinyu <1303842549@qq.com>
2020-09-14 03:21:55 +0800

    xiu改了

--- sss/src/com/bjpowernode/hello.java
+++ sss/src/com/bjpowernode/hello.java
@@ -3,5 +3,12 @@ package com.bjpowernode;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        //hello.test
+    }
+
+    public void test(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
     }
 }
